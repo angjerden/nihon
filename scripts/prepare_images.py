4aaafa53392b6185e98dd7914cef532bd6707a24
Anders Gjerde <angjerden@gmail.com>
2014-12-23 22:37:39 +0100

    Corrected sorting in images.js

--- scripts/prepare_images.py
+++ scripts/prepare_images.py
@@ -15,14 +15,12 @@ def rename_image_with_timestamp(dir, filename):
     img = Image.open(filepath)
 
     creation_time = get_creation_time(img)
-    print("\tImage creation time is {0}".format(creation_time))
     if filename.startswith('P'): #Only rename filenames starting with P
         new_filename = creation_time + "-" + filename
         new_filepath = dir + new_filename
         print("\tRenaming {0} to {1}".format(filename, new_filename))
         os.rename(filepath, new_filepath)
         filename = new_filename
-    print get_rotation(img)
     return filename
 
 def generate_images_js_entry(filename):
@@ -36,7 +34,8 @@ def generate_images_js_entry(filename):
 
 def resize_img_and_make_thumbnail(dir, filename):
     filename_without_extension = os.path.splitext(filename)[0]
-    new_filename = filename_without_extension + resized_suffix + ".jpg"
+    #new_filename = filename_without_extension + resized_suffix + ".jpg"
+    new_filename = filename #replacing with same filename
     generate_image_with_bounds(maxwidth, maxheight, filename, dir, new_filename)
 
     thumb_filename = filename_without_extension + thumb_suffix + ".jpg"
@@ -60,6 +59,7 @@ def generate_image_with_bounds(maxwidth, maxheight, filename, dir, new_filename)
 
     print("\tMaking {0} with size {1}x{2}".format(new_filename, new_width, new_height))
     if rotation is not None:
+        print("\tRotating with {} degrees".format(rotation))
         img = img.rotate(rotation, expand=True)
     img.thumbnail(new_image_size, Image.ANTIALIAS)
     img.save(new_filepath, "JPEG")#, quality=95)
@@ -86,15 +86,22 @@ def get_rotation(img):
 
 if __name__ == '__main__':
     dir = '../res/images/'
-    images_gen_js_file = open("../js/images.js", "w")
+    images_gen_js_file = open("../js/images_gen.js", "w")
     images_gen_js_string = "var images = [ \n"
 
     for filename in os.listdir(dir):
         if resized_suffix in filename or thumb_suffix in filename or \
             "jpg" not in filename.lower():
             continue
-        print(filename)
         filename = rename_image_with_timestamp(dir, filename)
+
+    filenames = os.listdir(dir)
+    filenames.sort() #sorting to get alphanumerical order
+    for filename in filenames:
+        if resized_suffix in filename or thumb_suffix in filename or \
+            "jpg" not in filename.lower():
+            continue
+        print("Resizing {}".format(filename))
         filename = resize_img_and_make_thumbnail(dir, filename)
         images_gen_js_string += generate_images_js_entry(filename)
 
